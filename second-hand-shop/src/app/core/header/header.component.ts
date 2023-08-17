2d2c5950e8ae02b51581e87a22db7ef2f842f6b1
Petar-Dinev <p.dinev55@gmail.com>
2023-08-17 21:40:48 +0300

    Add navigation in headerComponent plus css for header and footer components

--- second-hand-shop/src/app/core/header/header.component.ts
+++ second-hand-shop/src/app/core/header/header.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
 }
