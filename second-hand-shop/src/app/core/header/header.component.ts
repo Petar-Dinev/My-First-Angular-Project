bc55930a6fd17ad3eb18b8d3db0e30596e9d4551
Petar-Dinev <p.dinev55@gmail.com>
2023-08-18 23:17:17 +0300

    Add logic for navigation in header component

--- second-hand-shop/src/app/core/header/header.component.ts
+++ second-hand-shop/src/app/core/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,19 @@ import { ApiService } from 'src/app/api.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  
+  get isloggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+  get username(): string | undefined{
+    return this.userService.username;
+  }
+
+  constructor(private userService: UserService, private router: Router) {}
+
+
+  logout() {
+    this.userService.logOut();
+    this.router.navigate(['/'])
+  }
 }
